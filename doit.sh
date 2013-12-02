895aa8d3abb2ea4903e48bab1794f9e3c5ea6a11
Soren Hansen <Soren.Hansen@ril.com>
2013-12-02 15:03:37 +0100

    A couple of minor improvements

--- doit.sh
+++ doit.sh
@@ -2,14 +2,17 @@
 #
 # There are many ways to do this, but this is how I did it. - Soren
 #
-# Starting from a clean install of Ubuntu Saucy, this should do everything:
+# Starting from a clean install of Ubuntu Saucy,
+# this cares care of everything:
 #
 
-CACHE_CONTAINER=cacher
-DEVSTACK_CONTAINER=devstack
+CACHE_CONTAINER="${CACHE_CONTAINER:-cacher}"
+DEVSTACK_CONTAINER="${DEVSTACK_CONTAINER:-devstack}"
 
 function find_lxc_ip {
-    cont_name=$1
+    cont_name="$1"
+	out=""
+    # The container may not be up just quite yet.
     while ! echo "$out" | grep -q 10\.0\.3\.
     do
         out="$(sudo lxc-ls --fancy --fancy-format name,ipv4 ^${cont_name}\$ | tail -n 1)"
@@ -18,7 +21,7 @@ function find_lxc_ip {
 }
 
 function install_base_packages {
-    # Install lxc (duh)
+    # Install lxc
     sudo apt-get install lxc
 }
 
@@ -31,43 +34,61 @@ function find_devstack_ip {
 }
 
 function create_cache_container {
-    # Create and start the cacher container
+    # Create the cacher container
     sudo lxc-create -t ubuntu -n ${CACHE_CONTAINER} -- -r precise -b $USER
+    # Start it
     sudo lxc-start -d -n ${CACHE_CONTAINER}
 
+    # Find and set the cache_ip environment variable
 	find_cacher_ip
 
     # Log in over SSH and install devpi and apt-cacher-ng
-    ssh -t $cache_ip 'sudo apt-get -y install python-pip apt-cacher-ng ; sudo pip install devpi-server ; echo "@reboot devpi-server --host=0.0.0.0" | crontab -'
+    ssh -t $cache_ip 'sudo apt-get -y install python-pip apt-cacher-ng ; \
+	                  sudo pip install devpi-server ; \
+					  echo -e "PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/sbin:/bin\n@reboot devpi-server --host=0.0.0.0" | crontab -'
+
+
+    mkdir -p ${HOME}/.pip
+    echo "[global]" >> ${HOME}/.pip/pip.conf
+	echo "index-url = http://${cache_ip}:3141/root/pypi/+simple/" > ${HOME}/.pip/pip.conf
 }
 
 function create_devstack_container {
+    # Grab the cacher's IP if we don't already have it
 	find_cacher_ip
 
     # Create the devstack container
     sudo lxc-create -t ubuntu -n ${DEVSTACK_CONTAINER} -- -r precise -b $USER
 
-    # Allow for nested containers (specifically, we need the ability to twiddle
-    # with cgroups inside the devstack container)
-    sudo sed -e 's/^#\(lxc.aa_profile = lxc-co\)/\1/g' -e 's/^#\(lxc.hook.mount\)/\1/g' -i /var/lib/lxc/${DEVSTACK_CONTAINER}/config
-	echo '# nbd' | sudo tee --append /var/lib/lxc/${DEVSTACK_CONTAINER}/config
-	echo 'lxc.cgroup.devices.allow = c 43:* rwm' | sudo tee --append /var/lib/lxc/${DEVSTACK_CONTAINER}/config
+    # Allow for nested containers (specifically, we need the ability to
+    # twiddle with cgroups inside the devstack container)
+    sudo sed -e 's/^#\(lxc.aa_profile = lxc-co\)/\1/g' \
+	         -e 's/^#\(lxc.hook.mount\)/\1/g' -i \
+			 /var/lib/lxc/${DEVSTACK_CONTAINER}/config
+
+	echo -e '# nbd\necho 'lxc.cgroup.devices.allow = c 43:* rwm' | sudo tee --append /var/lib/lxc/${DEVSTACK_CONTAINER}/config
 
     # Fire it up and grab its IP
     sudo lxc-start -d -n ${DEVSTACK_CONTAINER}
-
 	find_devstack_ip
 
     # Log in and configure the APT proxy and add the havana cloud-archive
-    ssh -t $devstack_ip 'echo Acquire::Http::Proxy \"http://'${cache_ip}':3142/\"\; | sudo tee /etc/apt/apt.conf.d/90proxy ; sudo mkdir /dev/net ; sudo apt-get install python-software-properties ; sudo add-apt-repository cloud-archive:havana; sudo apt-get update ; sudo apt-get install git curl python-setuptools; sudo mkdir /var/run/openstack ; sudo chmod 777 /var/run/openstack'
+    ssh -t $devstack_ip 'echo Acquire::Http::Proxy \"http://'${cache_ip}':3142/\"\; | sudo tee /etc/apt/apt.conf.d/90proxy ;
+	                     sudo mkdir /dev/net ;
+						 sudo apt-get install python-software-properties ;
+						 sudo add-apt-repository cloud-archive:havana ;
+						 sudo apt-get update ;
+						 sudo apt-get install git curl python-setuptools ;
+						 sudo mkdir /var/run/openstack ;
+						 sudo chmod 777 /var/run/openstack ;
+                         sudo mkdir -p ${HOME}/.pip ;
+                         echo "[global]" | sudo tee --apend /root/.pip/pip.conf ;
+	                     echo "index-url = http://'${cache_ip}':3141/root/pypi/+simple/" | sudo tee --append /root/.pip/pip.conf'
 
     # Add kvm and tun ctrl devices
     sudo lxc-device -n ${DEVSTACK_CONTAINER} add /dev/kvm
     sudo lxc-device -n ${DEVSTACK_CONTAINER} add /dev/net/tun
 
-	echo "To use devpi:"
-    echo "   export PIP_INDEX_URL=http://${cache_ip}:3141/root/pypi/+simple/"
-	echo ""
 	echo "To access the devstack instance:"
     echo "   ssh ${devstack_ip}"
 }
